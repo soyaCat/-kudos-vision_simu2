03c95d9f396b93a0a7abfda13747d9661f7c38a2
soya <soyamilk432@gmail.com>
2021-02-25 23:59:05 +0900

    code optimization

--- play_game_ENV.py
+++ play_game_ENV.py
@@ -223,7 +223,7 @@ class Robot_Movement_argorithm():
         self.ball_scope_level = 0
         self.action = [3, 0, 0, 0]
         self.sidemoveCount = 0
-        self.mode_order_list = ["find_ball_and_set_robot_direct_to_ball"]
+        self.mode_order_list = ["find_ball_and_set_robot_direct_to_ball_and_approch_to_ball"]
 
     def manualmode(self):
         print("Manual Mode...")
@@ -251,7 +251,7 @@ class Robot_Movement_argorithm():
         #npnA[ballPosX, ballPosY, goalPosX, goalPosY]
         #action[3(조종모드), (2,4,6,8)키 패드에 따른 로봇 움직이기, 머리 팬(-360~360), 머리 틸트(-360~360)]
 
-        if mode == self.mode_order_list[0]:
+        if self.mode_order_list[mode] == self.mode_order_list[0]:
             if rpnA[0] == -1.0 and rpnA[1] == -1.0:
                 #볼이 없을 때
                 #초기화
@@ -367,7 +367,7 @@ if __name__ == "__main__":
             if finish_manual_mode == True:
                 RMA_mode = RMA_mode_list[1]
         elif RMA_mode == RMA_mode_list[1]:
-            action = RMA.set_action(received_position_npArr,"find_ball_and_set_robot_direct_to_ball")
+            action = RMA.set_action(received_position_npArr, 0)
         #action = [2,0,0,0]
         print(action)
         actionTuple = ConversionDataType.ConvertList2DiscreteAction(action,behavior_name)
